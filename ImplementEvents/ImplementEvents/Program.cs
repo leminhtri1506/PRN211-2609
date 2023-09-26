e460c4bd0b131741be12ed55a18e003d394f2487
leminhtri1506 <115976210+leminhtri1506@users.noreply.github.com>
2023-09-26 13:45:06 +0700

    Add files via upload

---
+++ ImplementEvents/ImplementEvents/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEventLINQ
+{
+    public delegate void PrintDeatails(string msg);
+
+    class Program
+    {
+        event PrintDeatails Print;
+        void Show(string msg) => Console.WriteLine(msg.ToUpper());
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Print += new PrintDeatails(p.Show);
+            p.Print("Hung keu GauGau ");
+            Console.ReadLine();
+        }
+    }
+    
+}
