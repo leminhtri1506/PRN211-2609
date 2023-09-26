0e9aaf23f3a47b446f7c3fc5386a829505a5aa8d
leminhtri1506 <115976210+leminhtri1506@users.noreply.github.com>
2023-09-26 13:47:38 +0700

    Add files via upload

---
+++ PassingDelegate/PassingDelegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEventLINQ
+{
+    public delegate void MyDelegate(string msg);
+    class MyClass
+    {
+        public static void Print(string message) => Console.WriteLine($"{message.ToUpper()}");
+        public static void Show(string message) => Console.WriteLine($"{message.ToLower()}");
+    }
+
+    class Program
+    {
+        static void InvokeDelegate(MyDelegate dele, string msg) => dele(msg);
+        static void Main(string[] args) 
+        {
+            string msg = "Passing Delegate As a Parameter";
+            InvokeDelegate(MyClass.Print, msg);
+            InvokeDelegate(MyClass.Show, msg);
+            Console.ReadLine();
+        }
+    }
+}
